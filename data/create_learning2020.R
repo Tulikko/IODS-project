289bef7ecf76572354d2a9bb783b4aa733010789
Uine Kailamäki <uine.kailamaki@gmail.com>
2020-11-04 23:23:14 +0200

    Data wrangling

---
+++ data/create_learning2020.R
@@ -0,0 +1,7 @@
+# Uine Kailamäki 04/11/2020 file for the IODS course
+
+#import data from web source, separator tab (\t), includes header
+lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+#read data
+lrn14
